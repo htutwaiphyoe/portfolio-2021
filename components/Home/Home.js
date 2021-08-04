3a866c43ebb33cb9fe15ecd3a86d1a59af48351a
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-08-04 07:15:53 +0630

    #add: project initialization

---
+++ components/Home/Home.js
@@ -0,0 +1,33 @@
+import classes from "./Home.module.css";
+import Typed from "react-typed";
+function Home(props) {
+    return (
+        <header className={classes.Home}>
+            <div>
+                <h1 className={classes.type}>
+                    Hi, I'm{" "}
+                    <Typed
+                        loop
+                        typeSpeed={60}
+                        backSpeed={40}
+                        strings={[
+                            "Htut Wai Phyoe.",
+                            "a web developer.",
+                            "a tech lover.",
+                            "a content writer.",
+                        ]}
+                        backDelay={1000}
+                        loopCount={0}
+                        showCursor
+                        cursorChar="|"
+                        // className={classes.type}
+                    />
+                </h1>
+
+                <p>I'm a </p>
+            </div>
+        </header>
+    );
+}
+
+export default Home;
