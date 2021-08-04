a31605bd17c50601903f78ed359dc9c9a4068dd5
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-08-04 18:27:47 +0630

    #add: home section

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,32 +1,91 @@
-import classes from "./Home.module.scss";
+import Link from "next/Link";
 import Typed from "react-typed";
+
+import classes from "./Home.module.scss";
 function Home(props) {
     return (
-        <header className={classes.Home}>
-            {/* <div>
-                <h1 className={classes.type}>
-                    Hi, I'm{" "}
-                    <Typed
-                        loop
-                        typeSpeed={60}
-                        backSpeed={40}
-                        strings={[
-                            "Htut Wai Phyoe.",
-                            "a web developer.",
-                            "a tech lover.",
-                            "a content writer.",
-                        ]}
-                        backDelay={1000}
-                        loopCount={0}
-                        showCursor
-                        cursorChar="|"
-                        // className={classes.type}
-                    />
-                </h1>
+        <section className={classes.home}>
+            <div className={`container ${classes.home__container}`}>
+                <div className={classes.home__content}>
+                    <div className={classes.home__social}>
+                        <a
+                            href="www.facebook.com/hwp.dev"
+                            className={classes.home__social__link}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <i
+                                className={`ri-facebook-circle-fill ${classes.home__social__icon}`}
+                            ></i>
+                        </a>
+                        <a
+                            href="www.facebook.com/hwp.dev"
+                            className={classes.home__social__link}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <i className={`ri-instagram-fill ${classes.home__social__icon}`}></i>
+                        </a>
+                        <a
+                            href="www.facebook.com/hwp.dev"
+                            className={classes.home__social__link}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <i className={`ri-twitter-fill ${classes.home__social__icon}`}></i>
+                        </a>
+                        <a
+                            href="www.facebook.com/hwp.dev"
+                            className={classes.home__social__link}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <i className={`ri-github-fill ${classes.home__social__icon}`}></i>
+                        </a>
+                    </div>
 
-                <p>I'm a </p>
-            </div> */}
-        </header>
+                    <figure className={classes.home__image}>
+                        <img src="images/logo/home3.png" alt="Home" />
+                    </figure>
+                    <div className={classes.home__data}>
+                        <h1 className={classes.home__title}>Hi, I'm HWP.</h1>
+                        <h2 className={classes.home__subtitle}>
+                            <Typed
+                                loop
+                                typeSpeed={60}
+                                backSpeed={40}
+                                strings={["Web developer", "FullStack Developer", "Content Writer"]}
+                                backDelay={1000}
+                                loopCount={0}
+                                showCursor
+                                cursorChar="|"
+                            />
+                        </h2>
+                        <p className={classes.home__description}>
+                            I write aritcles about web development especially for JavaScript and
+                            MERN Stack.
+                        </p>
+                        <Link href="/blogs">
+                            <a className={classes.home__btn}>
+                                Read blogs
+                                <i className={`ri-arrow-right-line ${classes.home__btn__icon}`}></i>
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+
+                <div className={classes.home__scroll}>
+                    <Link href="#about">
+                        <a className={classes.home__scroll__btn}>
+                            <i className={`ri-mouse-line ${classes.home__scroll__mouse}`}></i>
+                            <span className={classes.home__scroll__text}>Scroll down</span>
+
+                            <i className={`ri-arrow-down-fill ${classes.home__scroll__arrow}`}></i>
+                        </a>
+                    </Link>
+                </div>
+            </div>
+        </section>
     );
 }
 
--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,16 +1,31 @@
 import Link from "next/Link";
 import { useRouter } from "next/router";
+import { useRef, useEffect, useState } from "react";
 import classes from "./Navigation.module.scss";
 
 function Navigation(props) {
     const router = useRouter();
-
+    const themeIconRef = useRef();
     const items = [
         { path: "/", name: "Home" },
         { path: "/projects", name: "Projects" },
         { path: "/blogs", name: "Blogs" },
         { path: "/series", name: "Series" },
     ];
+
+    useEffect(() => {
+        const localTheme = localStorage.getItem("hwp-theme");
+        if (localTheme) {
+            document.body.classList[localTheme === "dark" ? "add" : "remove"]("dark-theme");
+            themeIconRef.current.classList[localTheme === "dark" ? "add" : "remove"]("ri-sun-line");
+        }
+    }, []);
+    function themeChangeHandler() {
+        document.body.classList.toggle("dark-theme");
+        themeIconRef.current.classList.toggle("ri-sun-line");
+        const newTheme = document.body.classList.contains("dark-theme") ? "dark" : "light";
+        localStorage.setItem("hwp-theme", newTheme);
+    }
 
     return (
         <header className={classes.header}>
@@ -40,7 +55,11 @@ function Navigation(props) {
                 </div>
 
                 <div className={classes.nav__btns}>
-                    <i className={`ri-moon-line ${classes.nav__icon}`}></i>
+                    <i
+                        className={`ri-moon-line ${classes.nav__icon}`}
+                        onClick={themeChangeHandler}
+                        ref={themeIconRef}
+                    ></i>
                 </div>
             </nav>
         </header>
