1073ddfeb69f9c63bc8e587cd57740c43ea61fc0
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-08-06 17:44:45 +0630

    #add: about section responsive

--- components/About/About.js
+++ components/About/About.js
@@ -27,7 +27,7 @@ function About(props) {
             <h2 className={`section__title`}>About me</h2>
             <span className={`section__subtitle`}>Introduction</span>
 
-            <div className={`container`}>
+            <div className={`container ${classes.about__container}`}>
                 <div className={`${classes.about__info}`}>
                     <img
                         src="images/logo/home3.svg"
@@ -35,12 +35,11 @@ function About(props) {
                         className={`${classes.about__image} load-hidden`}
                         id="about-image"
                     />
-                    <div>
+                    <div className={`${classes.about__data}`}>
                         <p id="about-text" className={`${classes.about__text} load-hidden`}>
-                            I'm currently a 21 years old university student studying at Mandalay
-                            Technological University specialized in Computer Engineering &
-                            Information Technology. I started coding during second year and
-                            apparently I'm pursuing Web Development.
+                            I'm currently a fourth year student studying CEIT at Mandalay
+                            Technological University. I started coding at second year and apparently
+                            I'm pursuing Web Development.
                         </p>
 
                         <a
