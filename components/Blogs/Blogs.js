2d7008aec9553740b23e9d12105498a6594fe300
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-08-10 12:05:22 +0630

    #add: showing text for no blogs found with specific topic

--- components/Blogs/Blogs.js
+++ components/Blogs/Blogs.js
@@ -1,14 +1,23 @@
 import classes from "./Blogs.module.scss";
 import Blog from "./Blog/Blog";
 import BlogTopic from "./BlogTopic/BlogTopic";
+
+import Button from "../UI/Button/Button";
 function Blogs(props) {
     return (
         <section className={`${classes.blogs} section container`}>
-            <div className={` ${classes.blogs__list}`}>
-                {props.blogs.map((blog, i) => (
-                    <Blog blog={blog} key={i} />
-                ))}
-            </div>
+            {props.blogs.length > 0 ? (
+                <div className={` ${classes.blogs__list}`}>
+                    {props.blogs.map((blog, i) => (
+                        <Blog blog={blog} key={i} />
+                    ))}
+                </div>
+            ) : (
+                <div className={classes.blogs__notfound}>
+                    <p className={classes.blogs__text}>No blogs with that topic</p>
+                    <Button type="link" text="Back to Blogs" href="/blogs" />
+                </div>
+            )}
             <div className={` ${classes.blogs__find}`}>
                 <p className={` ${classes.blogs__title}`}>Find with topic</p>
                 <div className={classes.blogs__topics}>
