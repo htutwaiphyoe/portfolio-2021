ce210227f04db78b0e15499e068e0af79bae284f
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-08-11 21:11:40 +0630

    #add: subscription email to subscriber and admin

--- controllers/subscriptionController.js
+++ controllers/subscriptionController.js
@@ -1,3 +1,5 @@
+import absoluteUrl from "next-absolute-url";
+
 import Subscription from "@/models/Subscription";
 import catchAsyncError from "@/middlewares/catchAsyncError";
 import sendEmail from "@/mail/email";
@@ -9,11 +11,19 @@ export const createSubscription = catchAsyncError(async (req, res, next) => {
         email,
         createdAt: Date.now(),
     });
+    const { origin } = absoluteUrl(req);
+    const link = `${origin}/blogs`;
+
+    await sendEmail({
+        email,
+        subject: "Welcome to my channel.",
+        body: `Thanks for subscribing. You'll receive a newsletter whenever a new blog is uploaded. For old blogs, you can read them here ${link}`,
+    });
 
     await sendEmail({
         email: "htutwaiphyoe@gmail.com",
         subject: "New subscription",
-        body: `${email} is subscribed for new blog. Send him email now.`,
+        body: `${email} is subscribed to my personal website.`,
     });
 
     res.status(201).json({
