87a71f3f9bb53d2ea8e2edb5a33e302c4e2f91fd
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-08-30 20:02:36 +0630

    #add: codigo project

--- components/Projects/Projects.js
+++ components/Projects/Projects.js
@@ -46,6 +46,12 @@ function Projects(props) {
             description: "Dynamic image grid built with ReactJS, Firebase and Google OAuth",
             types: ["WEBAPP"],
         },
+        {
+            image: "/images/site/projects/codigo.png",
+            title: "Codigo.co",
+            description: "Codigo Code Test project built with NextJS, ReactJS and Sass",
+            types: ["WEBSITE"],
+        },
     ];
     return (
         <section className={`container ${classes.projects}`}>
