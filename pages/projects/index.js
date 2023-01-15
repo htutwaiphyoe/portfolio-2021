0448ec9a48d670569781f80d4761b0f1fd6bfe38
htutwaiphyoe <htutwaiphyoe@gmail.com>
2023-01-15 13:29:16 +0630

    fix: subtitle to description

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -2,15 +2,15 @@ import Meta from "@/components/Meta/Meta";
 import Projects from "@/components/Projects/Projects";
 
 function ProjectsPage() {
-    return (
-        <>
-            <Meta
-                title="Projects - Htut Wai Phyoe"
-                subtitle="Htut Wai Phyoe. The projects I have done so far"
-            />
-            <Projects />
-        </>
-    );
+  return (
+    <>
+      <Meta
+        title="Projects - Htut Wai Phyoe"
+        description="The personal practice projects I have done so far in my current software development career."
+      />
+      <Projects />
+    </>
+  );
 }
 
 export default ProjectsPage;
